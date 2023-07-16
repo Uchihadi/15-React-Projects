413a7ffe21f325c73533e07e45f23f0ba89289d3
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-07-16 20:31:43 +0800

    React Project 1

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  // Invoking People Array from the data.js
+
+  return (
+  <main>
+    <section className='container'>
+      <h3> {people.length} Birthdays today </h3> 
+      <List people={people}/>
+      <button onClick={() => setPeople([])}> Clear All </button>
+      {/* Button when onClick is pressed, will set the people array to be empty,
+      as the useState already has Data inside
+      Once you reload, the people array will be back to being filled */}
+    </section>
+  </main>
+  )
 }
 
 export default App;
