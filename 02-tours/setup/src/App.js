81c080a56d61bd9153a2fb061c98848079292d3e
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-07-16 22:12:57 +0800

    Project 2 Update 34:17

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,8 +4,47 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project' //JSON Data from website
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // Start by setting up some state values
+  const [loading, setLoading] = useState(true); //Initialise loading as True
+  const [tours, setTours] = useState([]); // Initialise tours
+
+  const fetchTours = async () => {
+    setLoading(true); //the fetchTours will set loading as always true
+
+    try {
+    const response = await fetch(url); // Fetching data from the URL
+    const tours = await response.json(); //Response in JSON format
+    console.log(tours) //console log returns the JSON data from the url
+    setLoading(false);
+    setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+    
+  };
+
+  useEffect(() => { //Load up once in the window
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return(
+      <main>
+       <Loading />
+      </main>
+    )
+  } else {
+    return (
+      <main>
+        <Tours tours = {tours}/>
+      </main>
+    ) 
+  }
+
+  
 }
 
 export default App
